5bd72f4631d2632b0a1e34290d6928f6b5ba2b97
AakankshaBoral <44333965+AakankshaBoral@users.noreply.github.com>
2021-01-24 17:55:49 +0530

    Youtube player assignment

---
+++ Youtubleplayer/app/src/main/java/com/example/youtubleplayer/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.youtubleplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStyle;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+
+    private YouTubePlayerView youTubeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+
+        youTubeView.initialize(config.API_KEY, this);
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+        if (!b) {
+
+            // loadVideo() will auto play video
+            // Use cueVideo() method, if you don't want to play it automatically
+            youTubePlayer.loadVideo(config.YOUTUBE_VIDEO_CODE);
+
+            // Hiding player controls
+            youTubePlayer.setPlayerStyle(PlayerStyle.CHROMELESS);
+        }
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(
+                    getString(R.string.error_player), youTubeInitializationResult.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(config.API_KEY, this);
+        }
+    }
+
+    private YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    }
+}
---
+++ Youtubleplayer/app/src/main/java/com/example/youtubleplayer/config.java
@@ -0,0 +1,7 @@
+package com.example.youtubleplayer;
+
+public class config {
+
+    public static final String API_KEY = "AIzaSyBpEkBmRXwQcLF8x3hl7yCsVMbo4862sqY";
+    public static final String YOUTUBE_VIDEO_CODE = "https://www.youtube.com/watch?v=qqzy1EEid0E";
+}
